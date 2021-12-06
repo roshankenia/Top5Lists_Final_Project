793558ab383d06eca1737ce1c46698e8fb6f998e
Roshan Kenia <62453990+roshankenia@users.noreply.github.com>
2021-12-06 00:05:12 -0500

    Server Edits

--- server/controllers/top5list-controller.js
+++ server/controllers/top5list-controller.js
@@ -1,5 +1,37 @@
 const Top5List = require("../models/top5list-model");
+const CommunityList = require("../models/communitylist-model");
 const User = require("../models/user-model");
+
+createCommunityList = (req, res) => {
+  const body = req.body;
+  if (!body) {
+    return res.status(400).json({
+      errorMessage: "Improperly formatted request",
+    });
+  }
+
+  const communityList = new CommunityList(body);
+  console.log("creating communityList: " + JSON.stringify(communityList));
+  if (!communityList) {
+    return res.status(400).json({
+      errorMessage: "Improperly formatted request",
+    });
+  }
+
+  communityList
+    .save()
+    .then(() => {
+      return res.status(201).json({
+        communityList: communityList,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(400).json({
+        errorMessage: "CommunityList List Not Created!",
+      });
+    });
+};
 
 createTop5List = (req, res) => {
   const body = req.body;
@@ -39,6 +71,13 @@ createTop5List = (req, res) => {
     });
   });
 };
+
+deleteCommunityList = async (req, res) => {
+  CommunityList.findOneAndDelete({ _id: req.params.id }, () => {
+    return res.status(200).json({});
+  }).catch((err) => console.log(err));
+};
+
 deleteTop5List = async (req, res) => {
   console.log("delete Top 5 List with id: " + JSON.stringify(req.params.id));
   console.log("delete " + req.params.id);
@@ -148,6 +187,71 @@ getTop5Lists = async (req, res) => {
   }).catch((err) => console.log(err));
 };
 
+searchCommunityListByExactName = async (req, res) => {
+  const body = req.body;
+  if (!body) {
+    return res.status(400).json({
+      errorMessage: "Improperly formatted request",
+    });
+  }
+
+  const name = body.search;
+
+  await CommunityList.find(
+    { name: { $regex: new RegExp(name, "i") } },
+    (err, communityLists) => {
+      console.log("found CommunityLists: " + JSON.stringify(communityLists));
+      if (err) {
+        return res.status(400).json({ success: false, error: err });
+      }
+      if (!communityLists) {
+        console.log("!communityLists.length");
+        return res
+          .status(404)
+          .json({ success: false, error: "Community Lists not found" });
+      } else {
+        console.log("Send the CommunityLists");
+        return res
+          .status(200)
+          .json({ success: true, communityLists: communityLists });
+      }
+    }
+  ).catch((err) => console.log(err));
+};
+
+searchCommunityListByStart = async (req, res) => {
+  const body = req.body;
+  if (!body) {
+    return res.status(400).json({
+      errorMessage: "Improperly formatted request",
+    });
+  }
+
+  const name = body.search;
+  console.log(name);
+
+  await CommunityList.find(
+    { name: { $regex: new RegExp("^" + name, "i") } },
+    (err, communityLists) => {
+      console.log("found CommunityLists: " + JSON.stringify(communityLists));
+      if (err) {
+        return res.status(400).json({ success: false, error: err });
+      }
+      if (!communityLists) {
+        console.log("!communityLists.length");
+        return res
+          .status(404)
+          .json({ success: false, error: "Community Lists not found" });
+      } else {
+        console.log("Send the CommunityLists");
+        return res
+          .status(200)
+          .json({ success: true, communityLists: communityLists });
+      }
+    }
+  ).catch((err) => console.log(err));
+};
+
 searchTop5List = async (req, res) => {
   const body = req.body;
   if (!body) {
@@ -165,7 +269,7 @@ searchTop5List = async (req, res) => {
 
   if (listView === "yours") {
     await Top5List.find(
-      { username: username, name: { $regex: "^" + search } },
+      { username: username, name: { $regex: new RegExp("^" + search, "i") } },
       (err, top5Lists) => {
         console.log("found Top5Lists: " + JSON.stringify(top5Lists));
         if (err) {
@@ -184,7 +288,10 @@ searchTop5List = async (req, res) => {
     ).catch((err) => console.log(err));
   } else if (listView === "all") {
     await Top5List.find(
-      { published: { $eq: true }, name: { $regex: "^" + search } },
+      {
+        published: { $eq: true },
+        name: { $regex: new RegExp("^" + search, "i") },
+      },
       (err, top5Lists) => {
         console.log("found Top5Lists: " + JSON.stringify(top5Lists));
         if (err) {
@@ -202,22 +309,124 @@ searchTop5List = async (req, res) => {
       }
     ).catch((err) => console.log(err));
   } else if (listView === "users") {
-    await Top5List.find({published: { $eq: true }, username: search }, (err, top5Lists) => {
-      console.log("found Top5Lists: " + JSON.stringify(top5Lists));
-      if (err) {
-        return res.status(400).json({ success: false, error: err });
+    await Top5List.find(
+      {
+        published: { $eq: true },
+        username: { $regex: new RegExp(search, "i") },
+      },
+      (err, top5Lists) => {
+        console.log("found Top5Lists: " + JSON.stringify(top5Lists));
+        if (err) {
+          return res.status(400).json({ success: false, error: err });
+        }
+        if (!top5Lists) {
+          console.log("!top5Lists.length");
+          return res
+            .status(404)
+            .json({ success: false, error: "Top 5 Lists not found" });
+        } else {
+          console.log("Send the Top5Lists");
+          return res.status(200).json({ success: true, top5Lists: top5Lists });
+        }
       }
-      if (!top5Lists) {
-        console.log("!top5Lists.length");
-        return res
-          .status(404)
-          .json({ success: false, error: "Top 5 Lists not found" });
-      } else {
-        console.log("Send the Top5Lists");
-        return res.status(200).json({ success: true, top5Lists: top5Lists });
-      }
-    }).catch((err) => console.log(err));
+    ).catch((err) => console.log(err));
   }
+};
+
+updateCommunityList = async (req, res) => {
+  const body = req.body;
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide a body to update",
+    });
+  }
+
+  CommunityList.findOne({ _id: req.params.id }, (err, communityList) => {
+    console.log("communityList found: " + JSON.stringify(communityList));
+    if (err) {
+      return res.status(404).json({
+        err,
+        message: "CommunityList List not found!",
+      });
+    }
+
+    communityList.name = body.top5List.name;
+    communityList.items = body.top5List.items;
+    communityList.comments = body.top5List.comments;
+    communityList.likes = body.top5List.likes;
+    communityList.dislikes = body.top5List.dislikes;
+    communityList.views = body.top5List.views;
+    communityList.updateDate = body.top5List.updateDate;
+    communityList
+      .save()
+      .then(() => {
+        console.log("SUCCESS!!!");
+        return res.status(200).json({
+          success: true,
+          id: communityList._id,
+          message: "CommunityList List updated!",
+        });
+      })
+      .catch((error) => {
+        console.log("FAILURE: " + JSON.stringify(error));
+        return res.status(404).json({
+          error,
+          message: "CommunityList List not updated!",
+        });
+      });
+  });
+};
+
+updateTop5ListWithoutUser = async (req, res) => {
+  const body = req.body;
+  console.log("updateTop5List: " + JSON.stringify(body));
+  console.log(
+    "req.body.name, req.body.items: " + req.body.name + ", " + req.body.items
+  );
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide a body to update",
+    });
+  }
+
+  Top5List.findOne({ _id: req.params.id }, (err, top5List) => {
+    console.log("top5List found: " + JSON.stringify(top5List));
+    if (err) {
+      return res.status(404).json({
+        err,
+        message: "Top 5 List not found!",
+      });
+    }
+
+    top5List.name = body.top5List.name;
+    top5List.items = body.top5List.items;
+    top5List.comments = body.top5List.comments;
+    top5List.likes = body.top5List.likes;
+    top5List.dislikes = body.top5List.dislikes;
+    top5List.views = body.top5List.views;
+    top5List.published = body.top5List.published;
+    top5List.publishedDate = body.top5List.publishedDate;
+    top5List
+      .save()
+      .then(() => {
+        console.log("SUCCESS!!!");
+        return res.status(200).json({
+          success: true,
+          id: top5List._id,
+          message: "Top 5 List updated!",
+        });
+      })
+      .catch((error) => {
+        console.log("FAILURE: " + JSON.stringify(error));
+        return res.status(404).json({
+          error,
+          message: "Top 5 List not updated!",
+        });
+      });
+  });
 };
 
 updateTop5List = async (req, res) => {
@@ -302,4 +511,10 @@ module.exports = {
   getTop5Lists,
   updateTop5List,
   searchTop5List,
+  updateTop5ListWithoutUser,
+  updateCommunityList,
+  createCommunityList,
+  deleteCommunityList,
+  searchCommunityListByExactName,
+  searchCommunityListByStart,
 };
